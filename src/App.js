59740440a49693808762a3159a5b0c0f56d53e79
akuj <aku.jaakola@luukku.com>
2017-11-27 19:48:35 +0200

    Deleted extra files

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
 import React, { Component } from 'react';
-import TodoItem from './components/TodoItem';
-import TodoListHeader from './components/TodoListHeader';
-import Menu from './components/Menu';
-import NewItemInputs from './components/NewItemInputs';
-
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
-    const data = [
-      {
-        description: "Buy chocko",
-        dueDate: "22.11.2017",
-        type: "shop",
-        id: 1
-      },
-      {
-        description: "Learn React",
-        dueDate: "1.11.2017",
-        type: "learn",
-        id: 2
-      },
-      {
-        description: "Read book",
-        dueDate: "2.12.2017",
-        type: "read",
-        id: 3
-      }
-    ];
-
     return (
-      <div className="container">
-        <Menu />
-        <TodoListHeader />
-        <NewItemInputs showInputs={ false }/>
-        {
-          data.map(element => <TodoItem description={element.description}
-                                        dueDate={element.dueDate} 
-                                        type={element.type}
-                                        key={element.id} />)
-        }
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
