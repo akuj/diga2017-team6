0b318007ca9d8926ed6c6efae48b0296090c00ab
akuj <aku.jaakola@luukku.com>
2017-11-29 11:44:04 +0200

    Dropdown menus work

--- src/components/DropdownMenuScenarios.js
+++ src/components/DropdownMenuScenarios.js
@@ -1,57 +1,56 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Bootstrap, DropdownButton, MenuItem} from 'react-bootstrap';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import {Bootstrap, DropdownButton, MenuItem, Checkbox} from 'react-bootstrap';
 
 class DropdownMenuScenarios extends React.Component {
 
     constructor(props) {
         super(props);
     
-        this.toggle = this.toggle.bind(this);
-        this.select = this.select.bind(this);
+        this.changeTitle = this.changeTitle.bind(this);
         this.state = {
-          dropdownOpen: false
+            regionalLevels: ['Regional level 1', 'Regional level 2'],
+            regions: ['Region 1', 'Region 2'],
+            scenarioCollections: ['Scenario collection 1', 'Scenario collection 2'],
+            regionallevel: "default",
+            region: "default",
+            scenariocollection: "default"
         };
       }
 
-      handleSelect(event) {
-        console.log(event)
-    }
-
-    toggle() {
-        this.setState({
-          dropdownOpen: !this.state.dropdownOpen
-        });
-      }
-
-    select(event) {
-        this.setState({
-          dropdownOpen: !this.state.dropdownOpen,
-          value: event.target.innerText
-        });
+      changeTitle(evt)
+      {
+        this.setState({btnTitle: evt});
       }
 
     render () {
-        
         return (
             <div style={{textAlign:"left"}}>
                 <h1>Scenarios</h1>
                 <p>Regional level</p>
-                <DropdownButton title='Dropdowna' onSelect={function(evt){console.log(evt)}}>
-                    <MenuItem eventKey='Regional level 1'>Regional level 1</MenuItem>
-                    <MenuItem eventKey='Regional level 2'>Regional level 2</MenuItem>
-                </DropdownButton>   
+                <DropdownButton title={this.state.regionallevel} onSelect={(evt)=>{
+                    this.setState({regionallevel: evt})}}>
+                    <MenuItem eventKey={this.state.regionalLevels[0]}>{this.state.regionalLevels[0]}</MenuItem>
+                    <MenuItem eventKey={this.state.regionalLevels[1]}>{this.state.regionalLevels[1]}</MenuItem>
+                </DropdownButton>  
+                <p>  </p> 
                 <p>Region</p>
-                <DropdownButton title='Dropdownb' onSelect={function(evt){console.log(evt)}}>
-                    <MenuItem eventKey='Region 1'>Region 1</MenuItem>
-                    <MenuItem eventKey='Region 2'>Region 2</MenuItem>
+                <DropdownButton title={this.state.region} onSelect={(evt)=>{
+                    this.setState({region: evt})}}>
+                    <MenuItem eventKey={this.state.regions[0]}>{this.state.regions[0]}</MenuItem>
+                    <MenuItem eventKey={this.state.regions[1]}>{this.state.regions[1]}</MenuItem>
                 </DropdownButton>    
+                <p>  </p> 
                 <p>Scenario collection</p>
-                <DropdownButton title='Dropdownc' onSelect={function(evt){console.log(evt)}}>
-                    <MenuItem eventKey='Scenario collection 1'>Scenario collection 1</MenuItem>
-                    <MenuItem eventKey='Scenario collection 2'>Scenario collection 2</MenuItem>
-                </DropdownButton>     
+                <DropdownButton title={this.state.scenariocollection} onSelect={(evt)=>{
+                        this.setState({scenariocollection: evt})}}>
+                    <MenuItem eventKey={this.state.scenarioCollections[0]}>{this.state.scenarioCollections[0]}</MenuItem>
+                    <MenuItem eventKey={this.state.scenarioCollections[1]}>{this.state.scenarioCollections[1]}</MenuItem>
+                </DropdownButton>  
+                <p>  </p> 
+                <p>Scenarios</p>
+                <Checkbox>checkboxi</Checkbox>
+
             </div>
         )
     }
