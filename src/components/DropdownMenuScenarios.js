342c35cbc5abbe61af1f35af90f29a48d45fb0cf
akuj <aku.jaakola@luukku.com>
2017-11-29 14:00:48 +0200

    Checkboxes in progress

--- src/components/DropdownMenuScenarios.js
+++ src/components/DropdownMenuScenarios.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Bootstrap, DropdownButton, MenuItem, Checkbox} from 'react-bootstrap';
+import {Bootstrap, DropdownButton, MenuItem, ToggleButton, ToggleButtonGroup} from 'react-bootstrap';
+import {Button, ButtonGroup} from 'reactstrap'
+
+const regionalLevels = ['Regional level 1', 'Regional level 2'];
+const regions = ['Region 1', 'Region 2'];
+const scenarioCollections = ['Scenario collection 1', 'Scenario collection 2'];
+const scenarios = ['Scenario 1', 'Scenario 2'];
 
 class DropdownMenuScenarios extends React.Component {
 
     constructor(props) {
         super(props);
+        this.onCheckboxBtnClick = this.onCheckboxBtnClick.bind(this);
     
-        this.changeTitle = this.changeTitle.bind(this);
         this.state = {
-            regionalLevels: ['Regional level 1', 'Regional level 2'],
-            regions: ['Region 1', 'Region 2'],
-            scenarioCollections: ['Scenario collection 1', 'Scenario collection 2'],
-            regionallevel: "default",
-            region: "default",
-            scenariocollection: "default"
+            regionallevel: regionalLevels[0],
+            region: regions[0],
+            scenariocollection: scenarioCollections[0],
+            checkboxesSelected: []
         };
-      }
+    }
 
-      changeTitle(evt)
-      {
-        this.setState({btnTitle: evt});
-      }
+    onCheckboxBtnClick(selected) {
+        const index = this.state.check.indexOf(selected);
+        if (index < 0) {
+          this.state.checkboxesSelected.push(selected);
+        } else {
+          this.state.checkboxesSelected.splice(index, 1);
+        }
+        this.setState({ checkboxesSelected: [...this.state.checkboxesSelected] });
+    }
 
     render () {
         return (
@@ -30,26 +39,37 @@ class DropdownMenuScenarios extends React.Component {
                 <p>Regional level</p>
                 <DropdownButton title={this.state.regionallevel} onSelect={(evt)=>{
                     this.setState({regionallevel: evt})}}>
-                    <MenuItem eventKey={this.state.regionalLevels[0]}>{this.state.regionalLevels[0]}</MenuItem>
-                    <MenuItem eventKey={this.state.regionalLevels[1]}>{this.state.regionalLevels[1]}</MenuItem>
+                    <MenuItem eventKey={regionalLevels[0]}>{regionalLevels[0]}</MenuItem>
+                    <MenuItem eventKey={regionalLevels[1]}>{regionalLevels[1]}</MenuItem>
                 </DropdownButton>  
                 <p>  </p> 
                 <p>Region</p>
                 <DropdownButton title={this.state.region} onSelect={(evt)=>{
                     this.setState({region: evt})}}>
-                    <MenuItem eventKey={this.state.regions[0]}>{this.state.regions[0]}</MenuItem>
-                    <MenuItem eventKey={this.state.regions[1]}>{this.state.regions[1]}</MenuItem>
+                    <MenuItem eventKey={regions[0]}>{regions[0]}</MenuItem>
+                    <MenuItem eventKey={regions[1]}>{regions[1]}</MenuItem>
                 </DropdownButton>    
                 <p>  </p> 
                 <p>Scenario collection</p>
                 <DropdownButton title={this.state.scenariocollection} onSelect={(evt)=>{
                         this.setState({scenariocollection: evt})}}>
-                    <MenuItem eventKey={this.state.scenarioCollections[0]}>{this.state.scenarioCollections[0]}</MenuItem>
-                    <MenuItem eventKey={this.state.scenarioCollections[1]}>{this.state.scenarioCollections[1]}</MenuItem>
+                    <MenuItem eventKey={scenarioCollections[0]}>{scenarioCollections[0]}</MenuItem>
+                    <MenuItem eventKey={scenarioCollections[1]}>{scenarioCollections[1]}</MenuItem>
                 </DropdownButton>  
                 <p>  </p> 
                 <p>Scenarios</p>
-                <Checkbox>checkboxi</Checkbox>
+                <ToggleButtonGroup vertical type="checkbox">
+                    <ToggleButton value={1}>{scenarios[0]}</ToggleButton>
+                    <ToggleButton value={2}>{scenarios[1]}</ToggleButton>
+                </ToggleButtonGroup>
+
+                <h5>Checkbox Buttons</h5>
+        <ButtonGroup>
+          <Button color="primary" onClick={() => this.onCheckboxBtnClick(1)} active={this.state.checkboxesSelected.includes(1)}>One</Button>
+          <Button color="primary" onClick={() => this.onCheckboxBtnClick(2)} active={this.state.checkboxesSelected.includes(2)}>Two</Button>
+          <Button color="primary" onClick={() => this.onCheckboxBtnClick(3)} active={this.state.checkboxesSelected.includes(3)}>Three</Button>
+        </ButtonGroup>
+        <p>Selected: {JSON.stringify(this.state.checkboxesSelected)}</p>
 
             </div>
         )
