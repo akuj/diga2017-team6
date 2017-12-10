102df95c1c5570a711c1bcf5d45e7fec4c3f917e
akuj <aku.jaakola@luukku.com>
2017-12-11 00:26:17 +0200

    Updated variable names for Graphs props

--- src/App.js
+++ src/App.js
@@ -86,9 +86,9 @@ class App extends Component {
                                           sendChoicesToApp={this.getChoicesFromScenarioMenu}/>
                 </div>
               </div>
-              <div className="col-md-6"><Graphs regionobject = {this.state.region}
-                                                periodobject = {this.state.period} 
-                                                scenarioobject = {this.state.scenario}/>
+              <div className="col-md-6"><Graphs regionobject = {this.state.selectedRegion}
+                                                periodobject = {this.state.selectedPeriod} 
+                                                scenarioobject = {this.state.selectedScenarios}/>
               </div>
               <div className="col-md-3"><Indicators scenariosDataFromParent={this.state.scenariosData}
                                                     sendIndicatorChoicesToApp={this.getChoicesFromIndicatorMenu}/></div>
